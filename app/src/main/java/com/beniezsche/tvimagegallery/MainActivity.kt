0712efe3dd156cc66dfc9617f9a656112ccbb7da
bikraman <bikramankarmakar10@gmail.com>
2024-05-26 15:15:25 +0530

    changed generic name of viewmodel to something more specific

--- app/src/main/java/com/beniezsche/tvimagegallery/MainActivity.kt
+++ app/src/main/java/com/beniezsche/tvimagegallery/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.beniezsche.tvimagegallery
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.beniezsche.tvimagegallery.adapters.ImageAdapter
 import com.beniezsche.tvimagegallery.viewmodels.ImageViewModel
 import com.rubensousa.dpadrecyclerview.DpadRecyclerView
@@ -18,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viewModel = ViewModelProvider(this)[ImageViewModel::class.java]
+        val imageViewModel = ViewModelProvider(this)[ImageViewModel::class.java]
 
         val imageAdapter = ImageAdapter()
 
@@ -27,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         galleryGrid.adapter = imageAdapter
 
-        viewModel.getImages().observe(this, Observer {
+        imageViewModel.getImages().observe(this, Observer {
             imageAdapter.imageList.clear()
             imageAdapter.imageList.addAll(it)
             imageAdapter.notifyDataSetChanged()
